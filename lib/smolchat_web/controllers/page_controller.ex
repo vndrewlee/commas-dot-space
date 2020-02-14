6fdeb3319ff14ec172670c219f09433158443c76
vndrewlee <hello@vndrewlee.com>
2020-02-14 17:57:25 -0500

    add room controller functions

--- lib/smolchat_web/controllers/page_controller.ex
+++ lib/smolchat_web/controllers/page_controller.ex
@@ -2,6 +2,18 @@ defmodule SmolchatWeb.PageController do
   use SmolchatWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    conn
+    # |> put_flash(:info, "heyyyyyyyyyyyy")
+    # |> put_flash(:error, "Let's pretend we have an error.")
+    # |> put_layout("admin.html")
+    # |> assign(:id, "lobby")
+    |> render(:index)
   end
+
+  def show(conn, %{"id" => id}) do
+    conn
+    |> assign(:id, id)
+    |> render("room.html")
+  end
+
 end
