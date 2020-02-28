9ba33f0ec1caed7009b31e41b126a091f54ff8cf
vndrewlee <hello@vndrewlee.com>
2020-02-28 13:44:09 -0500

    user template for 404, not string

--- lib/smolchat_web/views/error_view.ex
+++ lib/smolchat_web/views/error_view.ex
@@ -10,6 +10,11 @@ defmodule SmolchatWeb.ErrorView do
   # By default, Phoenix returns the status message from
   # the template name. For example, "404.html" becomes
   # "Not Found".
+
+  # def render("404.html", _assigns) do
+  #   "Page not found"
+  # end
+  
   def template_not_found(template, _assigns) do
     Phoenix.Controller.status_message_from_template(template)
   end
